b20dad71a2e44e594d085b7c9ca99813a4d16321
Tim Davids <80420345+Tim219296219@users.noreply.github.com>
2021-04-01 23:20:37 +0200

    Add files via upload

---
+++ src/main/java/za/ac/cput/adpassignment/CalculatorsTest.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.adpassignment;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @219296219Tim Davids
+ */
+public class CalculatorsTest {
+    
+    public CalculatorsTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of main method, of class Calculators.
+     */
+    
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        Calculators.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of sumNum method, of class Calculators.
+     */
+    @Test
+    public void testSumNum() {
+        System.out.println("sumNum");
+        int num_A = 0;
+        int num_B = 0;
+        Calculators.sumNum(num_A, num_B);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    @Test
+    public void testEquality() {
+         fail("The test case is a prototype.");
+         assertEquals();
+    }
+    @Test
+    public void testIdentity() {
+        fail("The test case is a prototype."); /*fail*/
+    }
+
+    private void assertEquals() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
